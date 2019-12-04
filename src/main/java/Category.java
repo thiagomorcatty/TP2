fed3b1e1825bd609ef624d5a2112a59d1a2ed95e
joaocmgomes <a1291cavado@gmail.com>
2019-12-04 19:31:15 +0000

    Classe Category

---
+++ src/main/java/Category.java
@@ -0,0 +1,31 @@
+public class Category {
+    private String name;
+    private String description;
+    private String color;
+
+    public Category ( String name, String description, String color)
+
+    {
+        this.name = name;
+        this.description = description;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;     }
+    public void setName(String nameame){
+        this.name = name;     }
+
+    public String getDescription() {
+        return description;     }
+    public void setDescription(String description){
+        this.description = description;     }
+
+    public String getColor() {
+        return color;     }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+}
+
